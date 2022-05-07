6ccf91e2ad8ca6eedd5188b5ef5b2bd804bb4cc0
rafael cruz <rafael.linx@gmail.com>
2022-05-07 16:56:27 -0300

    UPDATE

--- hello.py
+++ hello.py
@@ -32,6 +32,8 @@ msg = "Hello World"
 if current_language == "pt_BR":
     msg = "Olá Mundo"
 elif current_language == "it_IT":
-    msg = "Ciao, Mondo"    
+    msg = "Ciao, Mondo" 
+elif current_language == "es_SP":
+    msg = "Hola, Mundo" 
 
 print(msg)
