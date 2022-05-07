97c425da4a0873703a9a0de64d8dde5d1047adfb
rafael cruz <rafael.linx@gmail.com>
2022-05-07 11:48:18 -0300

    Aula inicial

--- hello.py
+++ hello.py
@@ -1,3 +1,37 @@
-#Este programa imprime hello world
+#!/usr/bin/env python3
 
-print("hello world")
+"""Hello World Multi Linguas.
+
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem 
+correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+
+      export LANG=pt_BR
+      
+Execução:
+
+        python3 hello.py
+        ou
+        ./hello.py      
+"""
+
+__version__ ="0.0.1"
+__author__ ="Rafael Patrick"
+__license__ ="Unlicense"    
+
+import os
+
+current_language = os.getenv("LANG","en_US")[:5]
+# snake case
+
+msg = "Hello World"
+
+if current_language == "pt_BR":
+    msg = "Olá Mundo"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo"    
+
+print(msg)
